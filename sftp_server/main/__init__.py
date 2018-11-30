b6327801dfee2ce9faee8b38375ef538437482f0
xiaopeng-pyhon <wuxiaoyanhenni@163.com>
2018-11-30 18:16:15 +0800

    201811301816

--- sftp_server/main/__init__.py
+++ sftp_server/main/__init__.py
@@ -5,11 +5,3 @@ sys.path.append(Base_dir)
 
 __all__ = ['Json_handle']
 
-bb= {
-    "name": "xiaopeng",
-    "password": "76cd438bd0e8899a61aff632ace54dbb",
-    "path": "/home/xiaopeng",
-    "space": 0 }
-
-aa = Json_handle("xx",bb).ReadJson()
-print(aa)
