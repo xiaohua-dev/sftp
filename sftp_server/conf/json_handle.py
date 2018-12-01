de756ffff510a4ac37490f79b9c11157da42a6fd
xiaopeng-pyhon <wuxiaoyanhenni@163.com>
2018-12-01 22:19:08 +0800

    201812012219

--- sftp_server/conf/json_handle.py
+++ sftp_server/conf/json_handle.py
@@ -26,6 +26,6 @@ class Json_handle(object):
     def ReadJson(self):
         Json_file = open('%s/user_dir/%s.json' % (Base_dir,self.json_name), 'r+', encoding='utf-8')
         Read_file = json.load(Json_file)
-        js_name = Read_file["name"]
+        js_name = Read_file["user_name"]
         return Read_file
 
